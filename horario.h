4bd62bb0fe4a4e44e07081b4de97c7a21d84e3eb
Armando Hinojosa Ccama <sinpatrones1@gmail.com>
2020-12-28 02:38:43 -0500

    Se agregarón funciones a HORARIO.H, como para crear profesores, semestres, tipos de cursos, grupos de curso, tipos y grupos conjuntamente

--- horario.h
+++ horario.h
@@ -48,38 +48,99 @@ public:
         return true;
     }
 
-    size_t encontrarSemestre(string nombreSemestre){
-        for (size_t idx = 0; idx < this->semestres.size(); idx++){
+    void mostrarInfoHorario(){
+        cout << "-------- SEMESTRES ---------" << endl;
+        for (auto & semestre: this->semestres){
+            cout << semestre << ", ";
+        }
+        cout << endl << endl;
+        cout << "----- TIPOS DE CURSOS ------" << endl;
+        for (auto & tipos: this->tiposDeCursos){
+            cout << tipos << ", ";
+        }
+        cout << endl << endl;
+        cout << "---------- GRUPOS ----------" << endl;
+        for (auto & grupo: this->grupos){
+            cout << grupo << ", ";
+        }
+        cout << endl << endl;
+        cout << "-------- PROFESORES --------" << endl;
+        for (auto & profesor: this->profesores){
+            cout << profesor << ", ";
+        }
+        cout << endl << endl;
+        cout << "---------- CURSOS ----------" << endl;
+        for(auto & curso: this->cursos){
+            curso.second->mostrarValores();
+        }
+    }
+
+    int encontrarSemestre(string nombreSemestre){
+        for (int idx = 0; idx < this->semestres.size(); idx++){
             if (this->semestres[idx] == nombreSemestre)
                 return idx;
         }
         return -1;
     }
 
-    size_t encontrarProfesor(string nombreProfesor){
-        for (size_t idx = 0; idx < this->profesores.size(); idx++){
+    int encontrarProfesor(string nombreProfesor){
+        for (int idx = 0; idx < this->profesores.size(); idx++){
             if (this->profesores[idx] == nombreProfesor)
                 return idx;
         }
         return -1;
     }
 
-    size_t crearProfesor(string nombreProfesor){
-        size_t idProfesor = this->encontrarProfesor(nombreProfesor);
+    int encontrarGrupo(string nombreGrupo){
+        for (int idx = 0; idx < this->grupos.size(); idx++){
+            if (this->grupos[idx] == nombreGrupo)
+                return idx;
+        }
+        return -1;
+    }
+
+    int encontrarTipo(string nombreTipo){
+        for (int idx = 0; idx < this->tiposDeCursos.size(); idx++){
+            if (this->tiposDeCursos[idx] == nombreTipo)
+                return idx;
+        }
+        return -1;
+    }
+
+    int crearProfesor(string nombreProfesor){
+        int idProfesor = this->encontrarProfesor(nombreProfesor);
         if (idProfesor < 0){
             this->profesores.push_back(nombreProfesor);
-            return 0;
+            return this->profesores.size() - 1;
         }
         return idProfesor;
     }
 
-    size_t crearSemestre(string nombreSemestre){
-        size_t idSemestre = this->encontrarSemestre(nombreSemestre);
+    int crearSemestre(string nombreSemestre){
+        int idSemestre = this->encontrarSemestre(nombreSemestre);
         if (idSemestre < 0){
             this->semestres.push_back(nombreSemestre);
-            return 0;
+            return this->semestres.size() - 1;
         }
         return idSemestre;
+    }
+
+    int crearGrupo(string nombreGrupo){
+        int idGrupo = this->encontrarGrupo(nombreGrupo);
+        if (idGrupo < 0){
+            this->grupos.push_back(nombreGrupo);
+            return this->grupos.size() - 1;
+        }
+        return idGrupo;
+    }
+
+    int crearTipoCurso(string nombreCortoTipo){
+        int idTipoCurso = this->encontrarTipo(nombreCortoTipo);
+        if (idTipoCurso < 0){
+            this->tiposDeCursos.push_back(nombreCortoTipo);
+            return this->tiposDeCursos.size() - 1;
+        }
+        return idTipoCurso;
     }
 
     // Asignar profesor por nombre
@@ -111,6 +172,39 @@ public:
         if (this->cursos.find(nombreCortoCurso) == this->cursos.end()) // Si el curso no existe, devuelve false
             return false;
         this->cursos[nombreCortoCurso]->asignarSemestre(idSemestre);
+        return true;
+    }
+
+    bool crearSoloGrupoAlCurso(string nombreCortoCurso, string nombreCortoGrupo){
+        if (this->cursos.find(nombreCortoCurso) == this->cursos.end()) // Si el curso no existe, devuelve false
+            return false;
+        this->crearGrupo(nombreCortoCurso);
+        this->cursos[nombreCortoCurso]->crearGrupoCurso(nombreCortoGrupo);
+        return true;
+    }
+
+    bool crearSoloTipoAlCurso(string nombreCortoCurso, string nombreCortoTipo){
+        if (this->cursos.find(nombreCortoCurso) == this->cursos.end()) // Si el curso no existe, devuelve false
+            return false;
+        this->crearTipoCurso(nombreCortoTipo);
+        this->cursos[nombreCortoCurso]->crearTipoCurso(nombreCortoTipo);
+        return true;
+    }
+
+    bool crearGrupoAlTipo(string nombreCortoCurso, string tipo, string grupo){
+        if (this->cursos.find(nombreCortoCurso) == this->cursos.end()) // Si el curso no existe, devuelve false
+            return false;
+        this->cursos[nombreCortoCurso]->crearGrupoTipoCurso(tipo, grupo);
+        this->crearGrupo(grupo);
+        return true;
+    }
+
+    bool crearTipoAlGrupo(string nombreCortoCurso, string grupo, string tipo){
+        if (this->cursos.find(nombreCortoCurso) == this->cursos.end()) // Si el curso no existe, devuelve false
+            return false;
+        this->cursos[nombreCortoCurso]->crearTipoGrupoCurso(grupo, tipo);
+        this->crearTipoCurso(tipo);
+        this->crearGrupo(grupo);
         return true;
     }
 
--- main.cpp
+++ main.cpp
@@ -44,134 +44,29 @@ int main(){
     string octavoSemestre[] = {"IHC", "PFCI", "CPD", "SC", "FEBTI", "RH", "FC(e)", "TIS(e)"};
     string decimoSemestre[] = {"CC", "PFC2", "TI3", "IoT", "R(e)", "TEC(e)"};
 
-    Curso c1("ED2");
-    c1.crearTipoCurso("LA");
-    c1.crearTipoCurso("TE");
-
-    c1.crearGrupoTipoCurso("TE", "a");
-    c1.crearGrupoTipoCurso("TE", "b");
-    c1.crearTipoGrupoCurso("c", "TE");
-    c1.crearGrupoTipoCurso("LA", "a");
-    c1.crearGrupoTipoCurso("PR", "a");
-
-    c1.asignarProfesor(34);
-
-    c1.mostrarValores();
-/*
     Horario h1;
-    h1.crearSemestre("SegundoSemestre");
-    h1.crearSemestre("CuartoSemestre");
-    h1.crearSemestre("SextoSemestre");
-    h1.crearSemestre("OctavoSemestre");
-    h1.crearSemestre("DecimoSemestre");
 
-    h1.crearProfesor("Luis");
-    h1.crearProfesor("Pepe");
-    h1.crearProfesor("Jacinto");
-    h1.crearProfesor("Floripondio");
-    h1.crearProfesor("Gregorio");
+    h1.crearCurso("ED2");
+    h1.crearCurso("CC1");
+    h1.crearCurso("C1V");
+    h1.crearCurso("ITP2");
 
-    for(string & curso: segundoSemestre){
-        h1.crearCurso(curso, "SegundoSemestre");
-    }
-    for(string & curso: cuartoSemestre){
-        h1.crearCurso(curso, "CuartoSemestre");
-    }
-    for(string & curso: sextoSemestre){
-        h1.crearCurso(curso, "SextoSemestre");
-    }
-    for(string & curso: octavoSemestre){
-        h1.crearCurso(curso, "OctavoSemestre");
-    }
-    for(string & curso: decimoSemestre){
-        h1.crearCurso(curso, "DecimoSemestre");
-    }
+    h1.crearGrupoAlTipo("ED2", "TE", "a");
+    h1.crearGrupoAlTipo("ED2", "LA", "a");
+    h1.crearGrupoAlTipo("ED2", "TE", "b");
+    h1.crearGrupoAlTipo("ED2", "LA", "b");
+    h1.crearGrupoAlTipo("C1V", "TE", "a");
+    h1.crearGrupoAlTipo("C1V", "TE", "b");
+    h1.crearGrupoAlTipo("CC1", "TE", "b");
+    h1.crearGrupoAlTipo("CC1", "TE", "b");
 
-    cout << endl;
-    cout << h1.extraerGrupoCurso("ED2:a") << endl;
-    cout << h1.extraerGrupoCurso("ED2-TE") << endl;
-    cout << h1.extraerTipoCurso("ED2:c-LA") << endl;
-    cout << h1.extraerTipoCurso("ED2:c") << endl;
-    cout << h1.extraerTipoCurso("ED2-LA") << endl;
-    cout << h1.extrarNombreCurso("ED2:b") << endl;
-    cout << endl << endl;
+    h1.asignarProfesorAlCurso("ED2", "Luis");
+    h1.asignarProfesorAlCurso("CC1", "Carlos");
+    h1.asignarProfesorAlCurso("C1V", "Diego");
+    h1.asignarProfesorAlCurso("ITP2", "Luis");
 
-    h1.crearGrupoEnCurso("ED2");
-    h1.crearGrupoEnCurso("ED2");
-    h1.crearGrupoEnCurso("ITP2");
-    h1.crearGrupoEnCurso("ITP2");
-    h1.crearGrupoEnCurso("RN");
-    h1.crearGrupoEnCurso("RN");
-    h1.crearGrupoEnCurso("RN");
+    h1.mostrarInfoHorario();
 
-
-    h1.mostrarCursos();
-*/
-/*
-
-    Grafo<string> g1("coloracion", true);
-
-    for (auto & curso: primerSemestre){
-        for (auto & curso2: primerSemestre){
-            if (curso != curso2){
-                    g1.insertarArista(curso, curso2);
-            }
-        }
-    }
-
-    for (auto & curso: cuartoSemestre){
-        for (auto & curso2: cuartoSemestre){
-            if (curso != curso2){
-                g1.insertarArista(curso, curso2);
-            }
-        }
-    }
-
-
-    for (auto & curso: sextoSemestre){
-        for (auto & curso2: sextoSemestre){
-            if (curso != curso2){
-                g1.insertarArista(curso, curso2);
-            }
-        }
-    }
-
-    for (auto & curso: octavoSemestre){
-        for (auto & curso2: octavoSemestre){
-            if (curso != curso2){
-                g1.insertarArista(curso, curso2);
-            }
-        }
-    }
-
-    for (auto & curso: decimoSemestre){
-        for (auto & curso2: decimoSemestre){
-            if (curso != curso2){
-                g1.insertarArista(curso, curso2);
-            }
-        }
-    }
-
-    // string primerSemestre[] = {"ED2", "CC1", "C1V", "ITP2", "CI", "RN"};
-    // string cuartoSemestre[] = {"AED", "TC", "BD2", "ALN", "EP", "ECA"};
-    // string sextoSemestre[] = {"PC", "IS2", "EDA", "SO", "TI2", "MAC"};
-    // string octavoSemestre[] = {"IHC", "PFCI", "CPD", "SC", "FEBTI", "RH", "FC(e)", "TIS(e)"};
-    // string decimoSemestre[] = {"CC", "PFC2", "TI3", "IoT", "R(e)", "TEC(e)"};
-
-    g1.insertarArista("ED2","CC1");
-    g1.insertarArista("CC1","CPD");
-    g1.insertarArista("R(e)","SC");
-    g1.insertarArista("ED2","TC");
-    g1.insertarArista("PC","RH");
-    g1.insertarArista("PFC2","IHC");
-    g1.insertarArista("MAC","ECA");
-
-    g1.mostrarListaDeAdyacencia();
-
-    g1.colorearHorario(colores);
-
-    g1.crearArchivoDot();
-*/
 
 /*
     Grafo<string> g1("coloracion", true);
